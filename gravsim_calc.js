dbc030d88a8a071309aed8873998e27ba7a6eba3
tekuteku-life <kt9@live.jp>
2025-06-13 20:17:22 +0900

    Modify collision detection

--- gravsim_calc.js
+++ gravsim_calc.js
@@ -8,6 +8,86 @@ const TIME_SCALE = 1e3;
 const CALC_INTERVAL = 60;
 
 
+class GravSimObject {
+	constructor(id, x, y, vx, vy, ax, ay, mass, radius) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+		this.ax = ax;
+		this.ay = ay;
+		this.mass = mass;
+		this.radius = radius;
+	}
+	
+	getXt(dt) {
+		return this.x + this.vx*dt + 1/2*this.ax*dt*dt;
+	}
+	getYt(dt) {
+		return this.y + this.vy*dt + 1/2*this.ay*dt*dt;
+	}
+
+	getVXt(dt) {
+		return this.vx + this.ax*dt;
+	}
+	getVYt(dt) {
+		return this.vy + this.ay*dt;
+	}
+
+	getV() {
+		return Math.sqrt(this.vx*this.vx + this.vy*this.vy);
+	}
+	
+	applyGravity(other) {
+		const dx = other.x - this.x;
+		const dy = other.y - this.y;
+		const radiusSum = this.radius + other.radius;
+		const distSq = Math.max(dx * dx + dy * dy, radiusSum * radiusSum);
+		const dist = Math.sqrt(distSq);
+		const mass1 = this.mass;
+		const mass2 = other.mass;
+
+		const force = (G * mass1 * mass2) / distSq;
+		const accel = force / mass1;
+
+		this.ax += accel * dx / dist;
+		this.ay += accel * dy / dist;
+	}
+
+	isColliding(other, dt) {
+		const dx = other.x - this.x;
+		const dy = other.y - this.y;
+		const distSq = dx * dx + dy * dy;
+		const radiusSum = this.radius + other.radius;
+
+		if( distSq < radiusSum * radiusSum ) {
+			true;
+		}
+
+		const max_v1 = Math.max(Math.abs(this.getVXt(dt)), Math.abs(this.getVYt(dt)));
+		const max_v2 = Math.max(Math.abs(other.getVXt(dt)), Math.abs(other.getVYt(dt)));
+		const expandRadiusSum = radiusSum + (max_v1 + max_v2)*dt;
+		
+		if( distSq < expandRadiusSum * expandRadiusSum ) {
+			const EXPAND_DIV_NUM = 20;
+			for( let i = 1; i < EXPAND_DIV_NUM; i++ ) {
+				const dts = dt/EXPAND_DIV_NUM*i;
+				const dxs = other.getXt(dts) - this.getXt(dts);
+				const dys = other.getYt(dts) - this.getYt(dts);
+				const distSqs = dxs * dxs + dys * dys;
+				
+				if( distSqs < radiusSum * radiusSum ) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+}
+
+
 class GravSimCalc {
 	constructor() {
 		this.lastTime = 0;
@@ -25,17 +105,14 @@ class GravSimCalc {
 		const data = e.data;
 		switch (data.cmd) {
 			case 'add':
-				const newObj = {
-					id: data.id,
-					x: data.x,
-					y: data.y,
-					vx: data.vx || 0,
-					vy: data.vy || 0,
-					ax: data.ax || 0,
-					ay: data.ay || 0,
-					mass: data.mass || 1,
-					radius: data.radius || 1,
-				};
+				const newObj = new GravSimObject(
+					data.id,
+					data.x, data.y,
+					data.vx || 0, data.vy || 0,
+					data.ax || 0, data.ay || 0,
+					data.mass || 1,
+					data.radius || 1,
+				);
 				this.objects.push(newObj);
 				break;
 			case 'remove':
@@ -65,19 +142,6 @@ class GravSimCalc {
 		}
 	}
 
-	applyGravity(obj1, obj2) {
-		const dx = obj2.x - obj1.x;
-		const dy = obj2.y - obj1.y;
-		const distSq = Math.max(dx * dx + dy * dy, (obj1.radius + obj2.radius)*(obj1.radius + obj2.radius));
-		const dist = Math.sqrt(distSq);
-
-		const force = (G * obj1.mass * obj2.mass) / distSq;
-		const accel = force / obj1.mass;
-
-		obj1.ax += accel * dx / dist;
-		obj1.ay += accel * dy / dist;
-	}
-
 	convertToMessage() {
 		return this.objects.map(obj => {
 			return {
@@ -93,23 +157,14 @@ class GravSimCalc {
 		});
 	}
 
-	isColliding(obj1, obj2) {
-		const dx = obj2.x - obj1.x;
-		const dy = obj2.y - obj1.y;
-		const distSq = dx * dx + dy * dy;
-		const radiusSum = obj1.radius + obj2.radius;
-
-		return distSq < radiusSum * radiusSum;
-	}
-
-	collisionCheck() {
+	collisionCheck(dt) {
 		for (let i = 0; i < this.objects.length; i++) {
 			const obj = this.objects[i];
 			for (let j = i+1; j < this.objects.length; j++) {
 				const other = this.objects[j];
 
 				if (obj.id !== other.id) {
-					if( this.isColliding(obj, other) ) {
+					if( obj.isColliding(other, dt) ) {
 						if( obj.mass < other.mass ) {
 							obj.collided = true;
 						}
@@ -130,18 +185,16 @@ class GravSimCalc {
 			const other = this.objects[j];
 
 			if (obj.id !== other.id) {
-				this.applyGravity(obj, other);
+				obj.applyGravity(other);
 			}
 		}
 	}
 
-	update() {
-		const now = Date.now();
-		const dt = (now - this.lastTime) *YEARS_PER_SECOND /TIME_SCALE *this.timeScale;
-		this.lastTime = now;
-
+	moveObject(dt) {
 		for (let i = 0; i < this.objects.length; i++) {
 			const obj = this.objects[i];
+
+			if( obj.collided ) continue;
 
 			this.updateGravity(obj);
 			const half_vx = obj.vx + obj.ax * dt/2;
@@ -153,15 +206,22 @@ class GravSimCalc {
 			obj.vx = half_vx + obj.ax * dt/2;
 			obj.vy = half_vy + obj.ay * dt/2;
 
-			const v = Math.sqrt(obj.vx*obj.vx + obj.vy*obj.vy);
-			console.log(v);
+			const v = obj.getV();
 			if(v > C) {
 				obj.vx = C *(obj.vx/v);
 				obj.vy = C *(obj.vy/v);
 			}
 		}
 
-		this.collisionCheck();
+	}
+
+	update() {
+		const now = Date.now();
+		const dt = (now - this.lastTime) *YEARS_PER_SECOND /TIME_SCALE *this.timeScale;
+		this.lastTime = now;
+
+		this.moveObject(dt);
+		this.collisionCheck(dt);
 			
 		self.postMessage({
 			cmd: 'update',
